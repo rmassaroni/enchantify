f794ef7458c04f16046aaa268db612cd077216cf
rjm <robjmass@gmail.com>
2024-10-04 16:20:14 -0400

    cleanup

--- lua/enchantify.lua
+++ lua/enchantify.lua
@@ -1,63 +1,13 @@
-local enchant = {}
+local enchant_mapping = require("mapping")
+local get_usable_window_width = require("utils")
 
-local enchant_mapping = {
-    a = "ᔑ", b = "ʖ", c = "ᓵ", d = "↸", e = "ᒷ",
-    f = "⎓", g = "⊣", h = "⍑", i = "╎", j = "⋮",
-    k = "ꖌ", l = "ꖎ", m = "ᒲ", n = "リ", o = "𝙹",
-    p = "!¡", q = "ᑑ", r = "∷", s = "ᓭ", t = "ℸ ̣",
-    u = "⚍", v = "⍊", w = "∴", x = "̇", y = "/",
-    z = "||", [" "] = " ", ["\n"] = "\n"
-}
+local enchant = {}
 
 function enchant.translate_to_enchant(text)
     local translated = text:gsub(".", function(char)
         return enchant_mapping[char] or char
     end)
     return translated
-end
-
-local get_usable_window_width = function()
-    local window_width = vim.api.nvim_exec(
-    [[
-      function! BufferWidth()
-        let width = winwidth(0)
-        let numberwidth = max([&numberwidth, strlen(line('$')) + 1])
-        let numwidth = (&number || &relativenumber) ? numberwidth : 0
-        let foldwidth = &foldcolumn
-
-        if &signcolumn == 'yes'
-          let signwidth = 2
-        elseif &signcolumn =~ 'yes'
-          let signwidth = &signcolumn
-          let signwidth = split(signwidth, ':')[1]
-          let signwidth *= 2
-        elseif &signcolumn == 'auto'
-          let supports_sign_groups = has('nvim-0.4.2') || has('patch-8.1.614')
-          let signlist = execute(printf('sign place ' . (supports_sign_groups ? 'group=* ' : '') . 'buffer=%d', bufnr('')))
-          let signlist = split(signlist, "\n")
-          let signwidth = len(signlist) > 2 ? 2 : 0
-        elseif &signcolumn =~ 'auto'
-          let signwidth = 0
-          if len(sign_getplaced(bufnr(),{'group':'*'})[0].signs)
-            let signwidth = 0
-            for l:sign in sign_getplaced(bufnr(),{'group':'*'})[0].signs
-              let lnum = l:sign.lnum
-              let signs = len(sign_getplaced(bufnr(),{'group':'*', 'lnum':lnum})[0].signs)
-              let signwidth = (signs > signwidth ? signs : signwidth)
-            endfor
-          endif
-          let signwidth *= 2
-        else
-          let signwidth = 0
-        endif
-
-        return width - numwidth - foldwidth - signwidth
-      endfunction
-      echo BufferWidth()
-    ]],
-    true
-    )
-    return tonumber(window_width)
 end
 
 function enchant.display_translation()
@@ -104,7 +54,7 @@ function enchant.display_translation()
 
     vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
     vim.api.nvim_buf_set_option(buf, 'buflisted', false)
-    
+
     -- vim.api.nvim_buf_set_option(buf, 'modifiable', true)
     --
     -- vim.api.nvim_buf_set_lines(buf, 0, -1, false, translated_lines)
