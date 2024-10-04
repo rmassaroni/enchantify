f1b3c86260868cb736260ffad533ecba544097dc
rjm <robjmass@gmail.com>
2024-10-04 15:26:08 -0400

    fixed buffer size

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # enchantify
 
 ### TODO:
-- Fix bugged buffer size
 - Uppercase letters should be treated as lowercase
 - Remove deprecated code
 - Retain color
+- Keep file line at bottom
 
 
 
--- lua/enchantify.lua
+++ lua/enchantify.lua
@@ -16,6 +16,50 @@ function enchant.translate_to_enchant(text)
     return translated
 end
 
+local get_usable_window_width = function()
+    local window_width = vim.api.nvim_exec(
+    [[
+      function! BufferWidth()
+        let width = winwidth(0)
+        let numberwidth = max([&numberwidth, strlen(line('$')) + 1])
+        let numwidth = (&number || &relativenumber) ? numberwidth : 0
+        let foldwidth = &foldcolumn
+
+        if &signcolumn == 'yes'
+          let signwidth = 2
+        elseif &signcolumn =~ 'yes'
+          let signwidth = &signcolumn
+          let signwidth = split(signwidth, ':')[1]
+          let signwidth *= 2
+        elseif &signcolumn == 'auto'
+          let supports_sign_groups = has('nvim-0.4.2') || has('patch-8.1.614')
+          let signlist = execute(printf('sign place ' . (supports_sign_groups ? 'group=* ' : '') . 'buffer=%d', bufnr('')))
+          let signlist = split(signlist, "\n")
+          let signwidth = len(signlist) > 2 ? 2 : 0
+        elseif &signcolumn =~ 'auto'
+          let signwidth = 0
+          if len(sign_getplaced(bufnr(),{'group':'*'})[0].signs)
+            let signwidth = 0
+            for l:sign in sign_getplaced(bufnr(),{'group':'*'})[0].signs
+              let lnum = l:sign.lnum
+              let signs = len(sign_getplaced(bufnr(),{'group':'*', 'lnum':lnum})[0].signs)
+              let signwidth = (signs > signwidth ? signs : signwidth)
+            endfor
+          endif
+          let signwidth *= 2
+        else
+          let signwidth = 0
+        endif
+
+        return width - numwidth - foldwidth - signwidth
+      endfunction
+      echo BufferWidth()
+    ]],
+    true
+    )
+    return tonumber(window_width)
+end
+
 function enchant.display_translation()
     local current_buf = vim.api.nvim_get_current_buf()
     local buf = vim.api.nvim_create_buf(false, true)
@@ -29,8 +73,8 @@ function enchant.display_translation()
 
     vim.api.nvim_buf_set_lines(buf, 0, -1, false, translated_lines)
 
-    local win_width = vim.api.nvim_win_get_width(0)
-    local win_height = vim.api.nvim_win_get_height(0)
+    local win_width = get_usable_window_width()
+    local win_height = #lines
 
     local opts = {
         relative = 'editor',
@@ -39,7 +83,7 @@ function enchant.display_translation()
         row = 0,
         col = 0,
         style = 'minimal',
-        border = 'single'
+        border = 'none'
     }
     local win = vim.api.nvim_open_win(buf, true, opts)
 
@@ -55,4 +99,3 @@ function enchant.enchant_current_buffer()
 end
 
 return enchant
-
